7c928b5eb580b962cfa90ac49618fe7f4a754732
Przemek Mlodawski <przmlo09001@o2.pl>
2021-09-23 20:44:07 +0200

    New script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+
+
+var argButtonName, buttonNozyce, buttonKamien, buttonPapier;
+
+/**
+ * Describe this function...
+ */
+function buttonClicked(argButtonName) {
+  clearMessages();
+  console.log(argButtonName + ' został kliknięty');
+  var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
 
 /**
  * Describe this function...
@@ -37,12 +47,20 @@ printMessage ('Remis!');
   }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+playerMove = argButtonName;
 console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
 console.log('ruch gracza to: ' + playerMove);
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
+}
+
+buttonKamien = document.getElementById('button-rock');
+buttonPapier = document.getElementById('button-paper');
+buttonNozyce = document.getElementById('button-scissor');
+buttonKamien.addEventListener('click', function(){ buttonClicked('kamień'); });
+buttonPapier.addEventListener('click', function(){ buttonClicked('papier'); });
+buttonNozyce.addEventListener('click', function(){ buttonClicked('nożyce'); });
+
